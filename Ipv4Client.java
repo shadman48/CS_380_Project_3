af97da7ba0824307b235f8b557c216c0fad76125
LeeKann <ekannampuzha@cpp.edu>
2017-10-26 20:35:52 -0700

    Completed

--- Ipv4Client.java
+++ Ipv4Client.java
@@ -10,39 +10,62 @@ import java.util.Arrays;
 import java.nio.ByteBuffer;
 
 
+
 public class Ipv4Client {
      
-     public static void main(String[] args) throws Exception {
+     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("18.221.102.182", 38103);
+            Socket socket = new Socket("18.221.102.182", 38003);
             System.out.println("Connected to server.");
             InputStream is = socket.getInputStream();
-            DataInputStream dis = new DataInputStream(is);
+            //DataInputStream dis = new DataInputStream(is);
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
             OutputStream os = socket.getOutputStream();
             DataOutputStream dos = new DataOutputStream(os);
             
-            ByteBuffer ipv4 = new ByteBuffer();
+
             
             for(int i = 1; i <= 12; i++) {
+                
+                ByteBuffer ipv4 = ByteBuffer.allocate((int)(20 + Math.pow(2,i)));
+                
                 ipv4.put((byte)0x45);        // version + HLen
                 ipv4.put((byte)0x00);        // TOS
-                ipv4.put((byte)(20 + 2**i)); // total length
-                ipv4.put((byte)0x00);        // ident
-                ipv4.put((byte)0x00);        // ident
+                ipv4.putShort((short)(20 + Math.pow(2,i))); // total length
+                ipv4.putShort((short)0x00);        // ident
                 ipv4.put((byte)0x40);        // Flags
                 ipv4.put((byte)0x00);        // Offset
                 ipv4.put((byte)50);          // TTL
                 ipv4.put((byte)0x06);        // Protocol
                 
-                Bytebuffer noChecksumIpv4 = ipv4;     // temp bytebuffer for checksum
-                noChecksumIpv4.put((byte)0x0);        //
-                noChecksumIpv4.put((byte)0x0);        //
-                noChecksumIpv4.put(
-                ipv4.put(checksum(noChecksumIpv4);    //
+                ByteBuffer noChecksumIpv4 = ipv4.duplicate();     // temp bytebuffer for checksum
+                noChecksumIpv4.put((byte)0x0);        //  temp checksum
+                noChecksumIpv4.put((byte)0x0);        //  temp checksum
+                noChecksumIpv4.putInt((int)0x0);      // source address for temp
+                noChecksumIpv4.put((byte)18);         // destination address for temp
+                noChecksumIpv4.put((byte)221);        // .
+                noChecksumIpv4.put((byte)102);        // .
+                noChecksumIpv4.put((byte)182);        // .
                 
+                ipv4.putShort((short)checksum(noChecksumIpv4.array()));    // Checksum inserted
+                ipv4.putInt((int)0x0);      // source address for packet
+                ipv4.put((byte)18);         // destination address for packet
+                ipv4.put((byte)221);        // .
+                ipv4.put((byte)102);        // .
+                ipv4.put((byte)182);        // .
                 
+                for(int j = 0; j < Math.pow(2,i); j++) // data is just zeroes
+                    ipv4.put((byte)0x0);
+                
+                System.out.println("data length: " + (int)Math.pow(2,i));
+                dos.write(ipv4.array());
+                String check = br.readLine();
+                System.out.println(check + "\n");
             }
-            
+        }
+        catch(Exception e) {
+            e.printStackTrace();
         }
     }
     
